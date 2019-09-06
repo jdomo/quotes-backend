7a0c855f5f2b2987fc7d8326bf79e01b63e6f0dd
Joe Domovitch <jdomovitch@gmail.com>
2019-09-06 14:40:00 -0700

    render views in App.js

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Router, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Result from './components/Result';
 import Landing from './components/Landing';
 import Register from './components/Register';
 import Login from './components/Login';
 
-function App() {
+const my404 = () => {
   return (
-    <div className="App">
-      <Landing/>
+    <div>
+      Where do you think you're going?
     </div>
-  );
+  )
+}
+
+class App extends Component {
+  
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route exact path="/" render={(props) => <Landing {...props} />}/>
+          <Route component={my404} />
+        </Switch>
+      </main>
+    )
+  }
 }
 
 export default App;
