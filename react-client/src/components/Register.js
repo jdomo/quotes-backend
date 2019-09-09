69db36386df80a599f883608bb56ac998fe8f861
Joe Domovitch <jdomovitch@gmail.com>
2019-09-09 14:39:06 -0700

    register user working front/backend

--- react-client/src/components/Register.js
+++ react-client/src/components/Register.js
@@ -1,13 +1,55 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const Register = () => {
+  const [registerData, setRegisterData] = useState({
+    email: '',
+    password: '',
+    passwordCheck: ''
+  })
+
+  const onChangeHandler = (e) => {
+    setRegisterData({...registerData, [e.target.name]: e.target.value});
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (registerData.password !== registerData.passwordCheck) console.log('passwords don\'t match');
+    else {
+      const newUser = {
+        email: registerData.email,
+        password: registerData.password
+      }
+
+      try {
+
+        //body for backend post request
+        const body = await JSON.stringify(newUser);
+
+        const response = await fetch(`http://localhost:5000/api/users`, {
+          method: 'POST',
+          body: JSON.stringify(newUser),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+        const parsedUserResponse = await response.json()
+        console.log(parsedUserResponse, '<--- parsed user response containing JWT')
+
+      } catch (err) {
+        console.log(err, '<-- err from register onSubmit')
+      }
+    }
+  }
+
   return (
     <Fragment>
-      <h3>Register</h3>
+      <h4>Register</h4>
       <div className="container">
-        <form className="center-align" id="register-form">
-          <input type="email" placeholder="email" name="email"/>
-          <input type="password" placeholder="password" name="password"/>
+        <form className="form center-align" id="register-form" onSubmit={onSubmit}>
+          <input type="email" placeholder="email" name="email" value={registerData.email} onChange={onChangeHandler} required/>
+          <input type="password" placeholder="password" name="password" value={registerData.password} onChange={onChangeHandler} required/>
+          <input type="password" placeholder="re-type password" name="passwordCheck" value={registerData.passwordCheck} onChange={onChangeHandler} required/>
           <input type="submit" className="submit-btn btn" value="Create Account"/>
         </form>
       </div>
