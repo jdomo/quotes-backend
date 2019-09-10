8d948c7dc6b30822e29d0a509d9cf183f1c30c71
Joe Domovitch <jdomovitch@gmail.com>
2019-09-10 16:09:56 -0700

    deploy

---
+++ react-client/build/precache-manifest.85d046795a88dfb6eda6a7ef8237063a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970d6fb5113d4f0ba89c7f5627cfb9eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f96fddae1c4bf7e0471",
+    "url": "/static/css/main.9eb4663b.chunk.css"
+  },
+  {
+    "revision": "979f788bea37864cf0ab",
+    "url": "/static/js/2.d6cc23b9.chunk.js"
+  },
+  {
+    "revision": "7f96fddae1c4bf7e0471",
+    "url": "/static/js/main.3881418d.chunk.js"
+  },
+  {
+    "revision": "047350f8355b26a827ff",
+    "url": "/static/js/runtime~main.4a12003f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 connectDB();  //access config/db to connect to mongo
 
 //middleware
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'react-client', 'build')))
 app.use(express.json({extended: false})); //allows us to get data from req.body
 app.use(cors());
 
@@ -20,6 +23,11 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/quotes', require('./routes/api/quotes'))
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname,'react-client','build', 'index.html'))
+})
 
 //listener
 app.listen(PORT, () => {
